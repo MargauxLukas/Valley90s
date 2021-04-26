fe962064b3b6f0a955624596afe26c9dccc7c003
Timothe <linkazer@gmail.com>
2021-04-26 16:36:21 +0200

    IA Fonctionelle

---
+++ Valley/Assets/Scripts/Timothe/IA/Balise.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balise : MonoBehaviour
+{
+    private void OnDrawGizmos()
+    {
+        Color newCol = Color.yellow;
+        newCol.a = 0.2f;
+        UnityEditor.Handles.color = newCol;
+        UnityEditor.Handles.DrawWireDisc(transform.position, Vector3.up, 11);
+    }
+}
