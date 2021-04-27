6cf62235b2e52f21ee66a64b691f87ec3cc0c504
AlexC <alexandre.courtin.76@gmail.com>
2021-04-27 14:34:12 +0200

    Old Scoring

---
+++ Valley/Assets/Scripts/Alexandre/VisitorsFeedback.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisitorsFeedback : MonoBehaviour
+{
+    //Scripts Feedback Visitors (Dialogue + Memory Interest Point)
+    private LocationInfo locationInfos;
+
+}
