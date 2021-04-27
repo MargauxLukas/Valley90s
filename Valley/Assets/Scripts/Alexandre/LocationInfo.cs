656ac3941e313fe1596f8f155b8fab0a56b318fa
AlexC <alexandre.courtin.76@gmail.com>
2021-04-27 16:45:24 +0200

    Visitors Feedbacks

--- Valley/Assets/Scripts/Alexandre/LocationInfo.cs
+++ Valley/Assets/Scripts/Alexandre/LocationInfo.cs
@@ -2,8 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LocationInfo : MonoBehaviour
+public class LocationInfo : Balise
 {
     public string name;
-    public bool discovered = false;
+    public bool discovered = false;                 //Pour le joueur
+
+    public string dialoguePhrase;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.GetComponent<VisitorsFeedback>())
+        {
+            other.gameObject.GetComponent<VisitorsFeedback>().LandmarksPoint(this.GetComponent<LocationInfo>());
+        }
+    }
 }
